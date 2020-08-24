2285f2d1ae00fe06052974a7039d9106c3ca24d8
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-23 21:16:30 -0700

    add topnavbar

---
+++ nba-web/src/components/TopNavBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TopNavBar extends Component {
+    render() {
+        return (
+            <div>
+                This is header
+            </div>
+        );
+    }
+}
+
+export default TopNavBar;
