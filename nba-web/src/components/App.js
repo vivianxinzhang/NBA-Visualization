2a9ab0f02e53918fa1c4bc45068b0ff7c3682419
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-23 21:46:52 -0700

    update topbar style

--- nba-web/src/components/App.js
+++ nba-web/src/components/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import logo from "../assets/images/nba-logoman.svg";
 import TopNavBar from "./TopNavBar";
 // import logo from './logo.svg';
-import '../styles/App.css'
+// import '../styles/App.css'
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
         <TopNavBar></TopNavBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
     </div>
   );
 }
--- nba-web/src/components/TopNavBar.js
+++ nba-web/src/components/TopNavBar.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import logo from "../assets/images/nba-logoman.svg";
 
 class TopNavBar extends Component {
     render() {
         return (
-            <div>
-                This is header
-            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
         );
     }
 }
