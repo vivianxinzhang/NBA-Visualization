6e86cfddd932e20cd965aa46396f08c3f1975adc
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-25 21:32:11 -0700

    add profile component

--- nba-web/src/components/Main.js
+++ nba-web/src/components/Main.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Profile from "./Profile";
 
 class Main extends Component {
     render() {
         return (
             <div>
                 Main
+                <Profile />
             </div>
         );
     }
