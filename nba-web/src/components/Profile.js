08d7a99889ba9e9ea95f19a89ba10d89da3e604c
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-27 13:57:42 -0700

    display data on profile

--- nba-web/src/components/Profile.js
+++ nba-web/src/components/Profile.js
@@ -25,8 +25,41 @@ class Profile extends Component {
                 <img className="profile-picture"
                      src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
                      alt="Profile"/>
-                <div className="profile-entry player-name">
+                <div >
+                    <div >Team</div>
+                    <div>{`${teamCity} ${teamName}`}</div>
                     { playerName }
+                </div>
+                <img className="team-logo"
+                     src={`${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`}
+                     alt="Team"/>
+                <div >
+                    <div >Height</div>
+                    <div>{``}</div>
+                </div>
+                <div >
+                    <div ></div>
+                    <div>{`${height}`}</div>
+                </div>
+                <div >
+                    <div >Weight</div>
+                    <div>{`${weight}`}</div>
+                </div>
+                <div >
+                    <div >PTS</div>
+                    <div>{`${pts}`}</div>
+                </div>
+                <div >
+                    <div >REB</div>
+                    <div>{`${reb}`}</div>
+                </div>
+                <div >
+                    <div >AST</div>
+                    <div>{`${ast}`}</div>
+                </div>
+                <div >
+                    <div >PIE</div>
+                    <div>{`${pie}`}</div>
                 </div>
             </div>
         );
