93c7311e7f3a77e730a78de2a95db477c7a2b2c9
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-26 20:05:20 -0700

    save nba data in state

--- nba-web/src/components/Main.js
+++ nba-web/src/components/Main.js
@@ -1,7 +1,26 @@
 import React, {Component} from 'react';
 import Profile from "./Profile";
+import nba from '../nba-client';
 
 class Main extends Component {
+    // state = {
+    //     playerInfo: {},
+    //     playerId: 201939
+    // }
+
+    constructor() {
+        super();
+        this.state = {
+            playerInfo: {},
+            playerId: 201939
+        }
+    }
+
+    componentDidMount() {
+        window.nba = nba;  // 把 nba 变为全局变量
+
+    }
+
     render() {
         return (
             <div className="main">
