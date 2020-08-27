61403e5a5b84a56fc99699bc3197683b3aaad6e1
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-27 13:12:20 -0700

    pass data from Main to Profile

--- nba-web/src/components/Profile.js
+++ nba-web/src/components/Profile.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import { PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX } from '../constants';
 
 class Profile extends Component {
     render() {
+        const {
+            teamAbbreviation,
+            teamCity,
+            teamName,
+            playerName,
+            height,
+            weight,
+            playerId,
+            pts,
+            reb,
+            ast,
+            pie
+        } = this.props.playerInfo;
+
         return (
-            <div>
-                Profile
+            <div className="profile">
+                <div className="profile-entry player-name">
+                    { playerName }
+                </div>
+                <img className="profile-picture"
+                     src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+                     alt="Profile"/>
             </div>
         );
     }
