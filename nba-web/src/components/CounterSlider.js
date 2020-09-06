e24cc9b20aaebce2effd93a3a616b0afcd70e45a
Vivian Zhang <vivianxinzhang@gmail.com>
2020-09-05 21:39:48 -0700

    update slider

--- nba-web/src/components/CounterSlider.js
+++ nba-web/src/components/CounterSlider.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
+import { Slider, InputNumber, Row, Col } from 'antd';
 
 class CounterSlider extends Component {
+    state = {
+        inputValue: 1,
+    };
+
+    onChange = value => {
+        this.setState({
+            inputValue: value,
+        });
+    };
+
     render() {
+        const { inputValue } = this.state;
         return (
             <div>
                 CounterSlider
+                <Row>
+                    <Col span={12}>
+                        <Slider
+                            min={1}
+                            max={20}
+                            onChange={this.onChange}
+                            value={typeof inputValue === 'number' ? inputValue : 0}
+                        />
+                    </Col>
+                    <Col span={4}>
+                        <InputNumber
+                            min={1}
+                            max={20}
+                            style={{ marginLeft: 16 }}
+                            value={inputValue}
+                            onChange={this.onChange}
+                        />
+                    </Col>
+                </Row>
             </div>
         );
     }
